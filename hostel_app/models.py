505d62a51f00b0a33137fddc6431fe3e9159a3d2
sudee404 <sudaise18@gmail.com>
2023-03-21 23:21:08 +0300

    minor changes

--- hostel_app/models.py
+++ hostel_app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
-from hostel_app.admin import LocationAdmin
 # Create your models here.
 
 
